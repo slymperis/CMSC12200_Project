5cea05b10e8ac8635d6d6cbe10f136ec23bdbfa5
akahn2001 <64383145+akahn2001@users.noreply.github.com>
2022-03-07 12:14:58 -0600

    Add files via upload

--- portfolio_optimization.py
+++ portfolio_optimization.py
@@ -8,21 +8,6 @@ from correlation_modelling import forecast_var_cov_matrix
 from model_fitting import get_expected_returns
 from itertools import permutations
 
-expected_returns = np.matrix([[.1], [.1], [.04]]).T
-
-df = pd.read_csv("steve_data.csv")
-df = df.dropna()
-
-mapping = {}
-for i, col in enumerate([x for x in list(df.columns) if x != "Date"]):
-    mapping[i] = col
-
-#var_cov_matrix = np.matrix(forecast_var_cov_matrix(df))
-#num_assets = var_cov_matrix.shape[1]
-
-weights = [.5, .5]
-weights = np.array(weights).reshape(2, -1)
-
 def get_var_covar_matrix(df):
     """
     Calls the function in correlation_modelling.py that uses R to forecast the variance-covariance matrix
@@ -45,7 +30,7 @@ def get_portfolio_sharpe(var_cov_matrix, expected_returns, weights):
     sd = float(np.sqrt(variance))
     portfolio_er = float(expected_returns @ weights)
     sharpe = portfolio_er/sd
-    return sharpe
+    return sharpe, portfolio_er, sd
 
 def get_random_weights(num_assets):
     """
@@ -58,50 +43,63 @@ def get_random_weights(num_assets):
     weights = np.array(weights).reshape(num_assets, -1)
     return weights
 
-def portfolio_weights_monte_carlo(df, expected_returns, var_cov_matrix, iterations=5000000):
+def portfolio_weights_monte_carlo(df, expected_returns, var_cov_matrix, iterations=1000000):
     """
     Runs a Monte Carlo simulation to approximate optimal portfolio weights for an arbitrary number of assets
     :param df: DataFrame of log returns
     :param expected_returns: Array of expected returns
     :param var_cov_matrix: Forecasted variance-covariance matrix
-    :param iterations: Number of random weights to try (default is 5 million, takes about 2 minutes to run)
-    :return: Sharpe ratio, optimal weights (array) tuple
+    :param iterations: Number of random weights to try (default is 1 million, takes about 40 seconds to run)
+    :return: optimal weights (array), optimal portfolio expected return (Float), optimal portfolio
+     expected standard deviation (Float) tuple
     """
-    start_time = dt.datetime.now()
     num_assets = var_cov_matrix.shape[1]
     best_sharpe, best_weights = None, None
-    for i in range(5000000):
+    for i in range(iterations):
         weights = get_random_weights(num_assets)
-        sharpe = get_portfolio_sharpe(var_cov_matrix, expected_returns, weights)
+        sharpe, er, sd = get_portfolio_sharpe(var_cov_matrix, expected_returns, weights)
         if best_sharpe is None:
             best_sharpe = sharpe
             best_weights = weights
         elif sharpe > best_sharpe:
             best_sharpe = sharpe
             best_weights = weights
-    end_time = dt.datetime.now()
-    elapsed = end_time - start_time
-    print(elapsed.seconds)
-    return best_sharpe, best_weights
+    s, portfolio_er, portfolio_sd = get_portfolio_sharpe(var_cov_matrix, expected_returns, best_weights)
+    return best_weights, portfolio_er, portfolio_sd
 
-def main(model_specs):
+def main(model_specs, iterations=1000000):
+    """
+    Takes a dictionary mapping tickers to a tuple containing a set of ticker data to regress on,
+    a number of lags to use, a key to predict, a number of models to evaluate, a list of keywords to query
+    Google trends for, a start year, start month, end year, end month, and a boolean on whether or not to include
+    analyst recommendations
+    Fits models to best forecast expected returns and then forecasts the one day out of sample expected returns
+    matrix as well as the conditional variance-covariance matrix, approximates optimal portfolio weights and returns
+    them as a numpy array
+    :param model_specs: Dictionary
+    :param iterations: Optional, number of Monte Carlo iterations to perform
+    :return: NumPy array, float, float tuple
+    """
     expected_returns, log_returns = get_expected_returns(model_specs)
     var_covar_matrix = get_var_covar_matrix(log_returns.dropna())
-    optimal_weights = portfolio_weights_monte_carlo(log_returns, expected_returns, var_covar_matrix)
-    return optimal_weights
+    optimal_weights, portfolio_er, portfolio_sd = portfolio_weights_monte_carlo(log_returns,
+                                                        expected_returns, var_covar_matrix, iterations=iterations)
+    return optimal_weights, portfolio_er, portfolio_sd
 
-#print(portfolio_weights_monte_carlo(df, expected_returns, get_var_covar_matrix(df)))
+# Notes regarding the UI
+# "main" is what really needs to be called from a UI since the inputs are really long and it calls nearly everything in the project
+# basically we are inputting what assets we want in the portfolio, what we want each asset to be potentially modelled by, and we should get
+# recommended weights to hold for the next day
+# regarding the input dictionary: if a key is "AAPL" the ticker set in the tuple "AAPL" maps to must always include "AAPL" or it will throw an error
+# this means that if we are going to construct a portfolio with Apple stock we will always need Apple stock returns data
+# to forecast its expected returns
+# you can find more on what the really long inputs are doing in the functions get_expected_returns and get_data in model_fitting.py
+# note the out of sample forecast will be for whatever day comes after your end date, so if end year is 2022 and end month is 2 the out of sample
+# forecast would be for tomorrow which is what we generally want
 
+# SAMPLE USE OF MAIN
 #MODEL_SPECS = {"AAPL": ({"AAPL", "INTC", "MSFT"}, 3, "AAPL_log_return", 3, None, 2021, 5, 2022, 2, True),
  #              "GME": ({"GME", "AMC", "BB"}, 3, "GME_log_return", 3, None, 2021, 5, 2022, 2, True),
   #             "AMC": ({"AMC", "GME"}, 2, "AMC_log_return", 3, None, 2021, 5, 2022, 2, True)}
+#print(main(MODEL_SPECS))
 
-#expected_returns, log_returns = get_expected_returns(MODEL_SPECS)
-
-#var_covar_matrix = get_var_covar_matrix(log_returns.dropna())
-#print(portfolio_weights_monte_carlo(log_returns, expected_returns, var_covar_matrix))
-
-#print(get_portfolio_sharpe(var_cov_matrix, expected_returns, weights))
-
-#z = list(range(10))
-#c = combinations(z, 2)
