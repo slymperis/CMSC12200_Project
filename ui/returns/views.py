a8a5bccdba8ae606373142c038196f0795507fcc
Joyce Liu <joycelliu@uchicago.edu>
2022-03-08 18:46:54 -0600

    added code and rearranged files for UI

--- ui/returns/views.py
+++ ui/returns/views.py
@@ -13,16 +13,46 @@ from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
 
-#from portfolio_optimization import main
+from portfolio_optimization import main
 
 class SearchForm(forms.Form):
     stock_query = forms.CharField(
         label = 'Stock Ticker:', 
         help_text = 'e.g. AAPL', 
         required = True)
+    regress_ticker = forms.CharField(
+        label = 'Ticker Data to Regress On:',
+        help_text = 'e.g. AAPL, INTC, MSFT (separated by commas)',
+        required = True)
+    num_models = forms.CharField(
+        label = 'Number of Models to Evaluate:', 
+        help_text = 'e.g. 3', 
+        required = True)
+    num_lags = forms.CharField(
+        label = 'Number of Lags',
+        help_text = 'e.g. 3',
+        required = True)
     key_words = forms.CharField(
-        label = 'Key Words to Query:',
+        label = 'Key Words to Query for Google Trends:',
         help_text = 'e.g. iPhone',
+        required = False)
+    start_time_month = forms.CharField(
+        label = 'Start Time (Month)',
+        help_text = 'e.g. 3 (March)',
+        required = True)
+    start_time_year = forms.CharField(
+        label = 'Start Time (Year)',
+        help_text = 'e.g. 2021',
+        required = True)
+    end_time_month = forms.CharField(
+        label = 'End Time (Month)',
+        help_text = 'e.g. 5 (May)',
+        required = True) 
+    end_time_year = forms.CharField(
+        label = 'End Time (Year)',
+        help_text = 'e.g. 2022',
+        required = True)
+    analyst_recs = forms.BooleanField(label = 'Include Analyst Recommendations',
         required = False)
 
 def index(request):
@@ -30,8 +60,35 @@ def index(request):
     res = None 
     if request.method == 'GET':
         form = SearchForm(request.GET)
+        if form.is_valid():
+            args = {}
+            args_val = []
+            args_val.append({regress_ticker})
+            args_val.append(num_lags, stock_query + "_log_return", num_models)
+            if key_words:
+                args_val.append(key_words)
+            else:
+                args_val.append(None)
+            args_val.append(start_time_year, start_time_month, end_time_year, end_time_month)
+            if analyst_recs: 
+                args_val.append(True)
+            else:
+                args_val.append(False)
+    else:
+        form = SearchForm()
     
+    if res is None:
+        context['result'] = None
+    elif isinstance(res, str):
+        context['result'] = None
+        context['err'] = res
+        result = None
+    elif not _valid_result(res):
+        context['result'] = None
+        context['err'] = ('Return of portfolio_optimization has the wrong data type.')
+    else:
+        columns, result = res
+
     return render(request, 'index.html', context)
-    #return HttpResponse("You have arrived at the returns index for JAWS")
 
     
